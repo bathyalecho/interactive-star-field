adf5c05270b955ab7540898f9f9a8195ea016a44
kaxcara@gmail.com <kaxcara@gmail.com>
2025-12-23 10:16:57 -0500

    canvas size fixed to window size

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const x = Math.random() * canvas.width;
+const y = Math.random() * canvas.height;
+
+console.log('canvas width is: ', canvas.width, 'canvas height is: ', canvas.height);
+console.log('x is: ', x, 'y is: ', y);
+
+
+
 ctx.fillStyle = 'white';
 ctx.beginPath();
-ctx.arc(100, 100, 20, 0, Math.PI * 2);
+ctx.arc(x, y, 10, 0, Math.PI * 2);
 ctx.fill();
